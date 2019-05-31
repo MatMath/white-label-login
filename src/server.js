71edcc1b68e4fcf287b14fa541692bde687da669
Mathieu <mathieu.k.legault@gmail.com>
2019-05-31 18:32:46 -0400

    feat: 🎸 nultiple app under 1 login

--- src/server.js
+++ src/server.js
@@ -3,15 +3,14 @@ const express = require('express');
 
 const app = express();
 const DIST_DIR = __dirname;
-const HTML_FILE = path.join(DIST_DIR, '../dist', 'index.html');
-
-console.log('DIST_DIR:', DIST_DIR);
-console.log('HTML_FILE:', HTML_FILE);
+const login = path.join(DIST_DIR, '../dist', 'index.html');
+const app1 = path.join(DIST_DIR, '../dist', 'app1.html');
+const app2 = path.join(DIST_DIR, '../dist', 'app2.html');
 
 app.use(express.static(DIST_DIR));
-app.get('*', (req, res) => {
-    res.sendFile(HTML_FILE)
-});
+app.get('/app1', (req, res) => { res.sendFile(app1) });
+app.get('/app2', (req, res) => { res.sendFile(app2) });
+app.get('*', (req, res) => { res.sendFile(login) });
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
